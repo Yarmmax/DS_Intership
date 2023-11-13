6f3134a6f4c3c72fe935221251f408ed282768cd
Max <maxim_yarmaluk@mail.ru>
2023-11-13 15:49:33 +0300

    -Edit setup.py file

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='yarmmaxds',
-    version='0.1.0',
+    version='0.2.5',
     description='my test DS package',
     long_description=pathlib.Path("README.md").read_text(),
     long_description_content_type="text/markdown",
